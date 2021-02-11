4e2917e27d3120b07367ec91cf00857109a78302
Yernur Tengizay <ernur.tenizbay@mail.ru>
2021-02-11 23:55:53 +0600

    'init'

--- week2/Program.cs
+++ week2/Program.cs
@@ -19,7 +19,73 @@ namespace week2
             get;
             set;
         }
+        public Layer(DirectoryInfo dir,int pos)
+        {
+            this.dir = dir;
+            this.pos = pos;
+            this.content = new List<FileSystemInfo>();
+            content.AddRange(this.dir.GetDirectories());
+            content.AddRange(this.dir.GetFiles());
+        }
+        public void PrintInfo()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            int cnt = 0;
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                Console.WriteLine(d.Name);
+                cnt++;
+            }
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            foreach(FileInfo f in dir.GetFiles())
+            {
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                Console.WriteLine(f.Name);
+                cnt++;
 
+            }
+
+        }
+        public FileSystemInfo GetCurrentObject()
+        {
+            return content[pos];
+        }
+        public void SetNewPosition(int d)
+        {
+            if (d > 0)
+            {
+                pos++;
+            }
+            else
+            {
+                pos--;
+            }
+            if (pos >= content.Count)
+            {
+                pos = 0;
+            }
+            else if (pos < 0)
+            {
+                pos = content.Count - 1;
+            }
+        }
     }
     class Program
     {
@@ -27,24 +93,41 @@ namespace week2
         {
             F3();
         }
-        private static void PrintInfo(string path,int pos)
-        {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.White;
-            int cnt = 0;
-            foreach(DirectoryInfo d in dir.GetDirectory())
-            {
-
-            }
-          
-        }
+ 
         private static void F3()
         {
             Stack<Layer> history = new Stack<Layer>();
-            history.Push(new Layer { dir = new DirectoryInfo(@"C:\"), pos = 0 });
+            history.Push(new Layer(new DirectoryInfo(@"C:\"), 0));
             bool escape = false;
-            List<FileSystemInfo> fileSystemInfos = new List<FileSystemInfo>();
+            while (!escape)
+            {
+                Console.Clear();
+                history.Peek().PrintInfo();
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (history.Peek().GetCurrentObject().GetType() == typeof(DirectoryInfo))
+                        {
+                            history.Push(new Layer(history.Peek().GetCurrentObject() as DirectoryInfo, 0));
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SetNewPosition(-1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SetNewPosition(1);
+                        break;
+                    case ConsoleKey.Escape:
+                        
+                            history.Pop();
+                        if (history.Count == 0) escape = true;
+                            break;
+                        
+                    
+                }
+            }
+
         }
     }
 }
